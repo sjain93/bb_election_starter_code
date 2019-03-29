fbb2b0d3288081011103c957feca852f4684a3c3
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-29 15:42:30 -0400

    created list of candidates

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  let apiUrl = 'https://bb-election-api.herokuapp.com/';
+  axios.get(apiUrl)
+    .then((response) => {
+      let candidates = response.data.candidates.map(function(candidate) {
+        return {
+          name: candidate.name,
+          votes: candidate.votes
+        };
+      });
+      // make a handle to append the list items on to
+      let ul = document.createElement('ul');
+      document.body.appendChild(ul);
+      candidates.forEach(function(candidate){
+        let li = document.createElement("li");
+        li.innerText = `Name: ${candidate.name}, Votes: ${candidate.votes}`;
+        ul.appendChild(li);
+      });
+    });
 });
