1d4a46646fcc81d59a20e1c009e34639af950072
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-04-01 16:37:29 -0400

    assignment sort of completed

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -2,19 +2,37 @@ document.addEventListener("DOMContentLoaded", function() {
   let apiUrl = 'https://bb-election-api.herokuapp.com/';
   axios.get(apiUrl)
     .then((response) => {
-      let candidates = response.data.candidates.map(function(candidate) {
-        return {
-          name: candidate.name,
-          votes: candidate.votes
-        };
-      });
-      // make a handle to append the list items on to
       let ul = document.createElement('ul');
       document.body.appendChild(ul);
-      candidates.forEach(function(candidate){
+      response.data.candidates.forEach(function(candidate){
         let li = document.createElement("li");
         li.innerText = `Name: ${candidate.name}, Votes: ${candidate.votes}`;
         ul.appendChild(li);
+        let form = document.createElement("form");
+        let submit = document.createElement("button");
+        submit.innerText = "Vote!";
+        form.appendChild(submit);
+        li.appendChild(form);
+        form.method = "post";
+        form.action = "https://bb-election-api.herokuapp.com/vote";
+        let input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", "name");
+        input.setAttribute("value", `${candidate.name}`);
+        form.appendChild(input);
       });
-    });
+      document.addEventListener("submit", function(event){
+        event.preventDefault();
+        let postName = event.target[1].value;
+        axios.post("https://bb-election-api.herokuapp.com/vote", {"name":postName}).then((response)=> {
+          console.log(response);
+        });
+      });
+      let refresh = document.createElement("button");
+      refresh.innerText = "Refresh";
+      document.body.appendChild(refresh);
+      refresh.addEventListener("click", function(event){
+        document.location.reload();
+      });
+  });
 });
